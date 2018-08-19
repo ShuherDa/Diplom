62d570439b3a435f3aee5367b8efecab84935d0f
Zarubin Alexandr <shuherda@gmail.com>
2018-08-19 21:13:55 +0600

    Доработка

--- Diplom.py
+++ Diplom.py
@@ -8,7 +8,6 @@ from sys import getdefaultencoding
 MAIN_ID = 171691064
 # TOKEN = '7b23e40ad10e08d3b7a8ec0956f2c57910c455e886b480b7d9fb59859870658c4a0b8fdc4dd494db19099'
 TOKEN = 'b9ab9d541cf06f1caf5704d0102a2dcd166df7051f541ca8019438e58638b77d6daba53c6e6010406a79e'
-URL_LONG = 2000 #Нашел инфу что лучше использовать не длиннее 2000 символов, ном может быть чуть больше
 
 def vk_request(token, code):
     pprint('.')
@@ -27,32 +26,41 @@ def vk_request(token, code):
     else:
         return dict()
 
+def get_group(friends_group):
+    my_friends_group = []
+    for friends_id in friends_group:
+        if friends_group[friends_id] and 'items' in friends_group[friends_id]:
+            for group_id in friends_group[friends_id]['items']:
+                if not 'deactivated' in group_id.keys():
+                    if 'members_count' in group_id.keys():
+                        my_friends_group.append({'id': group_id['id'], 'name': group_id['name'],
+                                                 'members_count': group_id['members_count']})
+                    else:
+                        my_friends_group.append({'id': group_id['id'], 'name': group_id['name'],
+                                                 'members_count': 0})
+    return my_friends_group
+
+def get_code(friend):
+    code = f"return{{"
+    for id in friend:
+         code = f"{code}\"{id}\": API.groups.get({{\"user_id\":{id},\"fields\":\"members_count\",\"extended\":1}}),"
+    code = f"{code}}};"
+    return code
+
 def get_groups(friends):
     pprint('.')
     my_friends_group = []
-    code = 'return {'
-    url_len = 152 #длина начала url в функции vk_request
-    index = 0
-    for id in friends:
-        index += 1
-        part_code = '"%s": API.groups.get({"user_id":%s, "fields":"members_count", "extended":%s}),' % (id, id, 1)
-        code = '%s%s' % (code, part_code)
-        url_len += len(part_code)
-        if url_len >= URL_LONG or index == len(friends):
-            url_len = 152
-            code = '%s%s' % (code, '};')
-            friends_group = vk_request(TOKEN, code)
-            for friends_id in friends_group:
-                if friends_group[friends_id] and 'items' in friends_group[friends_id]:
-                    for group_id in friends_group[friends_id]['items']:
-                        if 'members_count' in group_id.keys():
-                            my_friends_group.append({'id': group_id['id'], 'name': group_id['name'],
-                                                     'members_count': group_id['members_count']})
-                        else:
-                            my_friends_group.append({'id': group_id['id'], 'name': group_id['name'],
-                                                     'members_count': 0})
-            code = 'return {'
+    for friend in parts(list(friends)):
+        code = get_code(friend)
+        friends_group = vk_request(TOKEN, code)
+        my_friends_group += get_group(friends_group)
+
     return my_friends_group
+
+def parts(lst, n=25):
+    """ разбиваем список на части - по 25 в каждой """
+    """Без разбивки почему то в исключение вываливается"""
+    return [lst[i:i + n] for i in iter(range(0, len(lst), n))]
 
 def write_json(filename, data):
     current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -62,7 +70,7 @@ def write_json(filename, data):
 
 def get_friends(id):
     print('.')
-    code = '%s%s' % ('return{', '"%s": API.friends.get({"user_id":%s})};' % (id, id))
+    code = f"return{{\"{id}\": API.friends.get({{\"user_id\":{id}}})}};"
     friends = vk_request(TOKEN, code)
     if str(id) in friends.keys():
         friends = friends[str(id)]
@@ -82,8 +90,8 @@ def get_file(my_groups, my_friends_group):
         for group in my_groups:
             if group['id'] == group_id:
                 new_group.append({'name': group['name'], 'gid': group['id'], 'members_count': group['members_count']})
-    return new_group
 
+    return new_group
 
 friends = get_friends(MAIN_ID)
 
